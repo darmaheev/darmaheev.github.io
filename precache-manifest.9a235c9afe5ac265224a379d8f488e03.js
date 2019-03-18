1ab20b00090367b15305814ee79cac647745a3b4
V.Darmakheev <v.darmakheev@playa.ru>
2019-03-19 00:04:44 +0800

    Updates

---
+++ precache-manifest.9a235c9afe5ac265224a379d8f488e03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "21062b874cb25d95f14e3b228858fa67",
+    "url": "/static/media/pokedex.21062b87.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ddc5fcafb89d9e296cb",
+    "url": "/static/js/main.959ede29.chunk.js"
+  },
+  {
+    "revision": "4c2ad2b52e7bb02538c6",
+    "url": "/static/js/2.6bca3bd5.chunk.js"
+  },
+  {
+    "revision": "7ddc5fcafb89d9e296cb",
+    "url": "/static/css/main.6b9aeccd.chunk.css"
+  },
+  {
+    "revision": "4c2ad2b52e7bb02538c6",
+    "url": "/static/css/2.42782791.chunk.css"
+  },
+  {
+    "revision": "8827b8a8dad216bd5a0821513c2abd72",
+    "url": "/index.html"
+  }
+];
--- src/api/authentication.js
+++
@@ -1,5 +0,0 @@
-const authentication = {
-  login: () => {}
-};
-
-export default authentication;
